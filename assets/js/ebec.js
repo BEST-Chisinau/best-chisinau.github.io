231a1d85b70d3ed83fbf7b40f274682b54ee6d65
vasiletaranovici <vasile.taranovici@gmail.com>
2018-01-15 14:28:33 +0200

    add blog widget through json

--- assets/js/ebec.js
+++ assets/js/ebec.js
@@ -3,11 +3,6 @@ function main() {
 
 (function () {
    'use strict';
-
-   /* ==============================================
-  	Testimonial Slider
-  	=============================================== */
-
   	$('a.page-scroll').click(function() {
         if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
           var target = $(this.hash);
@@ -27,10 +22,46 @@ function main() {
 }
 main();
 
-  	/*====================================
-    WOW JS
-    ======================================*/
 
-	// new WOW().init();
-	//smoothScroll
-	// smoothScroll.init();
+// blog
+$(function () {
+	var $content = $('#jsonContent');
+	var data = {
+		rss_url: 'https://medium.com/feed/@BEST_Chisinau'
+	};
+	$.get('https://api.rss2json.com/v1/api.json', data, function (response) {
+		if (response.status == 'ok') {
+			var output = '';
+			$.each(response.items, function (k, item) {
+				var visibleSm;
+				if(k < 3){
+					visibleSm = '';
+				 } else {
+					 visibleSm = ' visible-sm';
+				 }
+				output += '<div class="col-sm-6 col-md-4 wow fadeIn' + visibleSm + '">';
+				output += '<div class="blog-post"><header>';
+				// output += '<h4 class="date">' + $.format.date(item.pubDate, "dd<br>MMM") + "</h4>";
+				var tagIndex = item.description.indexOf('<img'); // Find where the img tag starts
+				var srcIndex = item.description.substring(tagIndex).indexOf('src=') + tagIndex; // Find where the src attribute starts
+				var srcStart = srcIndex + 5; // Find where the actual image URL starts; 5 for the length of 'src="'
+				var srcEnd = item.description.substring(srcStart).indexOf('"') + srcStart; // Find where the URL ends
+				var src = item.description.substring(srcStart, srcEnd); // Extract just the URL
+				output += '<a href="'+ item.link + '" target="_blank"><div class="blog-element"><img class="img-responsive" src="' + src + '""></div></a></header>';
+        // output += '<a href="'+ item.link + '" target="_blank"><div class="blog-element"><img class="img-responsive" src="' + src + '" width="360px" height="240px"></div></a></header>';
+				output += '<div class="blog-content"><h5><a href="'+ item.link + '">' + item.title + '</a></h5>';
+				output += '<div class="post-meta"><span>By ' + item.author + '</span></div>';
+				var yourString = item.description.replace(/<img[^>]*>/g,""); //replace with your string.
+				var maxLength = 200 // maximum number of characters to extract
+				//trim the string to the maximum length
+				var trimmedString = yourString.substr(0, maxLength);
+				//re-trim if we are in the middle of a word
+				trimmedString = trimmedString.substr(0, Math.min(trimmedString.length, trimmedString.lastIndexOf(" ")))
+				output += '<p>' + trimmedString + '...</p>';
+				output += '</div></div></div>';
+				return k < 3;
+			});
+			$content.html(output);
+		}
+	});
+});
